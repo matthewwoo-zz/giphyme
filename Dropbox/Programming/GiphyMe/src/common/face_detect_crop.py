c5b3b71dc74ed9fca6e41614d4a1871a1ac28ddc
matthewwoo <matthew.edan.woo@gmail.com>
2017-08-06 14:18:47 -0700

    Wrote script to paste seflie onto first giphy image

--- Dropbox/Programming/GiphyMe/src/common/face_detect_crop.py
+++ Dropbox/Programming/GiphyMe/src/common/face_detect_crop.py
@@ -17,13 +17,14 @@ def detect_face(face_file, max_results=4):
     return image.detect_faces()
 
 
-def outline_face(image, faces):
+def outline_face(faces):
     """Marks coordinate of face.
     """
     for face in faces:
         box = [(bound.x_coordinate, bound.y_coordinate)
                for bound in face.bounds.vertices]
     face_coordinates = [box[0][0],box[0][1], box[2][0], box[2][1]]
+    print face_coordinates
     return face_coordinates
 
 def crop_face(input_filename, face_coordinates, crop_filename):
@@ -44,16 +45,28 @@ def detect_crop(input_filename, output_filename, max_results):
         # Reset the file pointer, so we can read the file again
         image.seek(0)
 
-        face_coordinates = outline_face(image, faces)
+        face_coordinates = outline_face(faces)
         crop_face(input_filename, face_coordinates, output_filename)
 
+def outline_face_image(input_filename,max_results=1):
+    with open(input_filename, 'rb') as image:
+        faces = detect_face(image, max_results)
+        image.seek(0)
+        face_coordinates = outline_face(faces)
+        print face_coordinates
+    return face_coordinates
 
+def resize_image(input_filename,output_filename, face_coordinates):
+    height = face_coordinates[3] - face_coordinates[1]
+    print height
+    width = face_coordinates[2] - face_coordinates[0]
+    print width
+    img = Image.open(input_filename)
+    resize_img = img.resize((width, height), resample=0)
+    resize_img.save(output_filename)
 
-main('test.jpg', 'test_out.jpg', 1)
-
-
-
-
+# face_coordinates = [10,10,100,70]
+# resize_image('test_crop.jpg','test_resize.jpg',face_coordinates)
 
 
 
--- Dropbox/Programming/GiphyMe/src/common/paste_face.py
+++ Dropbox/Programming/GiphyMe/src/common/paste_face.py
@@ -1,4 +1,7 @@
-import src.common.face_sq_outline as fo
+from PIL import Image
+
+import src.common.face_detect_crop as fc
+import src.common.gif_maker as gm
 
 '''
 Single Paste
@@ -6,11 +9,24 @@ Single Paste
 - Split GIF into multiple frames
 - Identify face on GIF and get coordinates on first Frame
 - Normalize two images and resized cropped selfie
+  - Sample coordinates [192 (x1), 110(y1), 601(x2), 587(y2)]
+  - To calculate height
+        - (110-587)
+  - To calculate width
+        - (192-601)
 - Paste resized cropped seflie on GIF Image
--
 '''
 
 
-
+seflie_image = fc.detect_crop('test.jpg','test_crop.jpg',1)
+# gm.expand_gif('source.gif')
+gif_face_coordinates = fc.outline_face_image('gif0.png')
+print gif_face_coordinates[0]
+print gif_face_coordinates[1]
+resize_selfie_image = fc.resize_image('test_crop.jpg','test_resize.png',gif_face_coordinates)
+gif_image = Image.open('gif0.png').convert('RGBA')
+selfie = Image.open('test_resize.png').convert('RGBA')
+gif_image.paste(selfie, (gif_face_coordinates[0], gif_face_coordinates[1],gif_face_coordinates[2],gif_face_coordinates[3]))
+gif_image.save('giphy_me_0.png')
 
 
